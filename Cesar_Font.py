71daa42342ec4bf51f758291511e41db58751c94
Vadim <Vagor3000@yandex.ru>
2021-10-19 22:35:16 +0300

    Программа поволяет делать выбор между двух языков, шифровать или декодировать, а так же менять шаг сдвига при необхоимости не выходя из программы

--- Cesar_Font.py
+++ Cesar_Font.py
@@ -1,14 +1,46 @@
+print()
 
 
-
-def encode_en2(x):
+def encode_rus(step, x):
     y = ''
-    step = 0
+    for i in range(len(x)):
+        if x[i].isalpha():
+            if x[i].isupper():
+                if (ord(x[i]) + step) < 1072:
+                    y += chr(ord(x[i]) + step)
+                else:
+                    y += chr(ord(x[i]) + step - 32)
+            else:
+                if (ord(x[i]) + step) < 1104:
+                    y += chr(ord(x[i]) + step)
+                else:
+                    y += chr(ord(x[i]) + step - 32)
+        else:
+            y += x[i]
+    print(y)
 
-    for k in range(len(x)):
-        if x[k].isalpha():
-            step += 1
 
+def decode_rus(step, x):
+    y = ''
+    for i in range(len(x)):
+        if x[i].isalpha():
+            if x[i].isupper():
+                if (ord(x[i]) - step) > 1039:
+                    y += chr(ord(x[i]) - step)
+                else:
+                    y += chr(ord(x[i]) - step + 32)
+            else:
+                if (ord(x[i]) - step) > 1071:
+                    y += chr(ord(x[i]) - step)
+                else:
+                    y += chr(ord(x[i]) - step + 32)
+        else:
+            y += x[i]
+    print(y)
+
+
+def encode_en(step, x):
+    y = ''
     for i in range(len(x)):
         if x[i].isalpha():
             if x[i].isupper():
@@ -23,16 +55,53 @@ def encode_en2(x):
                     y += chr(ord(x[i]) + step - 26)
         else:
             y += x[i]
-    return y
+    print(y)
 
 
-x = input().split()
-k = []
+def decode_en(step, x):
+    y = ''
+    for i in range(len(x)):
+        if x[i].isalpha():
+            if x[i].isupper():
+                if (ord(x[i]) - step) > 64:
+                    y += chr(ord(x[i]) - step)
+                else:
+                    y += chr(ord(x[i]) - step + 26)
+            else:
+                if (ord(x[i]) - step) > 96:
+                    y += chr(ord(x[i]) - step)
+                else:
+                    y += chr(ord(x[i]) - step + 26)
+        else:
+            y += x[i]
+    print(y)
 
-for i in range(len(x)):
-    k.append(str(encode_en2(x[i])))
 
-print(*k)
+nap = input('Введите направление (шифрование или дешифрование):')
+lan = input('Введите язык ввода (en, rus):')
+x = input('Введите ваше сообщение:')
 
+while True:
+    step = int(input('Введите шаг сдвига:'))
 
+    if nap.lower() == 'шифрование' or 'encode':
+        if lan.lower() == 'rus':
+            encode_rus(step, x)
+        elif lan.lower() == 'en':
+            encode_en(step, x)
+        else:
+            print('При выборе языка была допущена ошибка')
+    elif nap.lower() == 'дешифрование' or 'decodeR':
+        if lan.lower() == 'rus':
+            decode_rus(step, x)
+        elif lan.lower() == 'en':
+            decode_en(step, x)
+        else:
+            print('При выборе языка была допущена ошибка')
+    else:
+        print('При выборе направления была допущена ошибка')
 
+    m = input('Хотите изменить шаг сдвига?')
+    if m.lower() == 'нет' or m.lower() == 'no':
+        print("Спасибо, что воспользовались шифратором Цезаря!")
+        break
